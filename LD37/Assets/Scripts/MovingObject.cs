69c16b8017e620ad33d7de2701d6be48eb73a500
bes-kurales <besn1989@gmail.com>
2016-12-10 16:14:52 +0700

    Prefabs and move

---
+++ LD37/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MovingObject : MonoBehaviour {
+    public float speed = 6f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private Vector3 movement;
+
+    // Use this for initialization
+    protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+	
+	 protected void Move(float h, float v)
+    {
+        if (h == 0 && v == 0)
+            return;
+
+        movement.Set(h, v, 0f);
+        
+        movement = movement.normalized * speed * Time.deltaTime;
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, transform.position + movement, blockingLayer);
+
+
+        if (hit.transform == null)
+            rb2D.MovePosition(transform.position + movement);
+    }
+}
---
+++ LD37/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MovingObject {
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Awake () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate  () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        RaycastHit2D hit;
+        // Move the player around the scene.
+        Move(h, v);
+
+    }
+}
